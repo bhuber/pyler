94b07ae4fe523690f26d26774f300e0e4c3f68d4
Bennet Huber <bhuber@azavea.com>
2012-07-11 19:59:29 -0400

    Solved problem 12

--- py/primes.py
+++ py/primes.py
@@ -5,11 +5,31 @@ This is meant to be a module for dealing with primes
 """
 
 import math
+import pdb
 
 class PrimeGenerators(object):
     '''
     Class for defining generators for prime numbers
     '''
+
+    _primes = None
+
+    def __init__(self):
+        self._primes = [2]
+    
+    def __getitem__(self, nth_prime):
+        if nth_prime < len(self._primes):
+            return self._primes[nth_prime]
+
+        
+        #pdb.set_trace()
+        i = self._primes[-1] + 1
+        while nth_prime >= len(self._primes):
+            i = PrimeGenerators._add_prime(self._primes, i)
+            i += 1
+
+        return self._primes[nth_prime]
+
 
     @staticmethod
     def first_n_primes(n):
@@ -25,8 +45,22 @@ class PrimeGenerators(object):
         primes = [2]
         i = 3
         while len(primes) < n:
-            i = _add_prime(primes, i)
+            i = PrimeGenerators._add_prime(primes, i)
             yield i
+            i += 1
+
+    @staticmethod
+    def all_primes():
+        '''
+        Generates prime numbers indefinitely
+        '''
+
+        primes = [2]
+        i = 3
+        while True:
+            i = PrimeGenerators._add_prime(primes, i)
+            yield i
+            i += 1
 
     @staticmethod
     def primes_less_than(n):
@@ -65,6 +99,66 @@ class PrimeGenerators(object):
                 return i
 
             i += 1
+
+
+class PrimeMethods(object):
+    '''
+    Defines some common functions involving prime numbers
+    '''
+
+    @staticmethod
+    def n_factors(divisors):
+        '''
+        divisors is a dictionary, where key is a prime factor and value is its exponent
+        for example, the dictionary for 18 would be { 2: 1, 3: 2 }
+        '''
+
+        #see http://en.wikipedia.org/wiki/Divisor_function
+        keys_plus_1 = map(lambda a: a + 1, divisors.values())
+        if len(keys_plus_1) > 1:
+            return reduce(lambda a, b: a * b, keys_plus_1)
+        else:
+            return len(keys_plus_1)
+
+    @staticmethod
+    def get_prime_factors(n):
+        '''
+        Given n, find its prime factors
+        returns a dictionary where keys are primes, values are exponents
+        '''
+
+        sqrt_n = math.sqrt(n)
+        prime_max = sqrt_n
+
+        i = 0
+        p = 2
+        result = { }
+        while p < prime_max and p < sqrt_n and n > 1:
+            p = PrimeMethods._prime_gen[i]
+            i += 1
+            keep_trying = True
+
+            while keep_trying:
+                if n % p == 0:
+                    n /= p
+                    sqrt_n = math.sqrt(n)
+                    if not result.has_key(p):
+                        result[p] = 1
+                    else:
+                        result[p] += 1
+                else:
+                    keep_trying = False
+
+        #last value of n was a prime, so add it
+        if n != 1:
+            result[n] = 1
+
+        return result
+
+
+
+    _prime_gen = PrimeGenerators()
+
 
 
     
