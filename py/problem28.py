1ea3ce61fad74fd5b1f566687f1ea1589bad54b9
Bennet Huber <bhuber@azavea.com>
2012-07-14 23:47:41 -0400

    Touching up problem 28

--- py/problem28.py
+++ py/problem28.py
@@ -26,26 +26,34 @@ def problem28(n):
     f(n) = ((n_2)^2 + 1*n_1) + ((n_2)^2 + 2*n_1) + ((n_2)^2 + 3*n_1) + ((n_2)^2 + 4*n_1)
          = 4 * (n_2)^2 + 10 * n_1
 
-     check: 
+    check: 
         13 + 17 + 21 + 25 = 76
         4 * (3 * 3) + 10 * 4 = 76
 
     let g(n) = sum of diagonals of an nxn spiral
-    g(n) = f(n) + f(n - 2) + ... + f(1)
-         = f(1) + f(3) + ... + f(n)
-         = 1 + (4 * 1^2 + 10 * 2) + (4 * 3^2 + 10 * 4) + ... + 
-            (4 * k_i^2 + 10 + k_(i + 1)) + ... + (4 * n_2^2 + 10 * n_1)
-         = 1 + sum(4 * (2i)^2 + 10 * (2i + 1), i, i = 1, i < floor(n/2))
-         = 1 + 4 * sum((odd numbers less than n) squared) + 10 * sum(even numbers less than n)
+             = f(n) + f(n - 2) + ... + f(1)
+             = f(1) + f(3) + ... + f(n)
+             = 1 + (4 * 1^2 + 10 * 2) + (4 * 3^2 + 10 * 4) + ... + 
+                 (4 * k_i^2 + 10 + k_(i + 1)) + ... + (4 * n_2^2 + 10 * n_1)
+             = 1 + sum(4 * (2i)^2 + 10 * (2i + 1), i, i = 1, i < floor(n/2))
+             = 1 + 4 * sum((odd numbers less than n) squared) + 10 * sum(even numbers less than n)
 
-    let s(n) = sum of i <= n
-    s(n) = n * (n + 1) / 2
-    let ss(n) = sum of squares <= n
-    ss(n) = n * (n + 1) * (2 * n + 1) / 6
-    let ses(n) = sum of even squares <= n = sum((2*i)^2, i = 1, i <= n/2) = 4 * sum(i^2, i = 1, i <= n/2) 
-             = 4 * ss(n / 2)
-    let sos(n) = sum of odd squares <= n = ss(n) - ses(n)
-    let e(n) = sum of even i <= n = 2 * s(n / 2)
+    let s(n)    = sum of i <= n
+                = n * (n + 1) / 2
+
+    let ss(n)   = sum of squares <= n
+                = n * (n + 1) * (2 * n + 1) / 6
+
+    let ses(n)  = sum of even squares <= n 
+                = sum((2*i)^2, i = 1, i <= n/2) 
+                = 4 * sum(i^2, i = 1, i <= n/2) 
+                = 4 * ss(n / 2)
+
+    let sos(n)  = sum of odd squares <= n
+                = ss(n) - ses(n)
+
+    let e(n)    = sum of even i <= n 
+                = 2 * s(n / 2)
 
     g(n) = 1 + 4 * sos(n - 1) + 10 * e(n - 1)
     """
@@ -58,14 +66,10 @@ def sos(n): return ss(n) - ses(n)
 def e(n): return 2 * s(n / 2)
 def g(n): return 1 + 4 * sos(n - 1) + 10 * e(n - 1)
 
-
-
 def main():
     #print("Hello World!")
     print(problem28(1001))
 
 
 main()
-
-
 
