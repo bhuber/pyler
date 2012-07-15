3e2460ddcbfaa3d0cc6c17954ffe448fe3183ac3
Bennet Huber <bhuber@azavea.com>
2012-07-14 23:37:52 -0400

    Touch up problem 27

--- py/problem27.py
+++ py/problem27.py
@@ -10,7 +10,7 @@ n^2 + n + 41
 
 It turns out that the formula will produce 40 primes for the consecutive values n = 0 to 39. However, when n = 40, 40^2 + 40 + 41 = 40(40 + 1) + 41 is divisible by 41, and certainly when n = 41, 41^2 + 41 + 41 is clearly divisible by 41.
 
-Using computers, the incredible formula  n^2  79n + 1601 was discovered, which produces 80 primes for the consecutive values n = 0 to 79. The product of the coefficients, 79 and 1601, is 126479.
+Using computers, the incredible formula  n^2 -79n + 1601 was discovered, which produces 80 primes for the consecutive values n = 0 to 79. The product of the coefficients, 79 and 1601, is 126479.
 
 Considering quadratics of the form:
 
@@ -26,17 +26,17 @@ from primes import PrimeGenerators
 def quad(n, a, b):
     return n * n + a * n + b
 
-def problem27(limit):
+def problem27(alow, ahigh, bhigh):
     pg = PrimeGenerators()
     prime_set = set()
     prime_max = 0
     i_p = 0
-    best = [0, 0, 0]
+    best = [0, None, None]      #[n, a, b]
 
-    for a in xrange(-limit, limit + 1):
+    for a in xrange(alow, ahigh):
         i_b = 0
         b = pg[i_b]
-        while b < limit:
+        while b < bhigh:
             n = 0
             val = quad(n, a, b)
             while val > prime_max:
@@ -56,15 +56,10 @@ def problem27(limit):
 
     return best
 
-            
-
-
-    
-
 
 def main():
     #print("Hello World!")
-    print(problem27(1000))
+    print(problem27(-1000, 1001, 1001))
 
 
 main()
